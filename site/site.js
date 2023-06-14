3d3fdb45ed759a8780fa65e93bd7c60d1afb9de1
JP_Ribeiro <103456562+joaoP-ribeiro@users.noreply.github.com>
2023-06-14 09:52:57 -0300

    Delete site directory

--- site/site.js
+++
@@ -1,7 +0,0 @@
-const navbar = document.querySelector(".navbar")
-    const menuBotao = document.querySelector(".botao_resp")
-
-    menuBotao.addEventListener("click", () =>{
-        navbar.classList.toggle("menu")
-    })
-
